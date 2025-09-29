09cca2631d5d70b0adfa8abe6070266cecdabb7c
Antonio Todarello-web <antonytodarello@gmail.com>
2025-09-29 15:56:19 +0200

    finish

--- scripts/D1.js
+++ scripts/D1.js
@@ -9,40 +9,46 @@ REGOLE
 
 /* ESERCIZIO 1
  Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
-*/
+ */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+/* string (testo), number (numeri), boolean (valori booleani, true o false), null (valore speciale che indica l'assenza intenzionale di un valore), undefined (valore assegnato a una variabile non inizializzata) e symbol (identificatori unici)*/
 
 /* ESERCIZIO 2
  Crea una variable chiamata "myName" e assegna ad essa il tuo nome, sotto forma di stringa.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+/* cons myName='Antonio'*/
 
 /* ESERCIZIO 3
  Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+/* let n1=12
+   let n2=20
+   let s1= n1 + n2
+   */
 
 /* ESERCIZIO 4
  Crea una variable di nome "x" e assegna ad essa il numero 12.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+/* let x = 12 */
 
 /* ESERCIZIO 5
   Riassegna un nuovo valore alla variabile "myName" già esistente: il tuo cognome.
   Dimostra l'impossibilità di riassegnare un valore ad una variabile dichiarata con il costrutto const.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+/* const myName='Todarello'*/
 
 /* ESERCIZIO 6
  Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+/* let sottrazioneDiDueNnumeri = 4 
+   let x2= 12
+   let rest1= 4 - x
+*/
 
 /* ESERCIZIO 7
  Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
@@ -50,4 +56,10 @@ REGOLE
  EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+/*
+let name1='john'
+let name2='John'
+let name3='jonj' !== 'John'
+let name4= John.toLowerCase()
+let name5='name1' === 'name4'
+*/
